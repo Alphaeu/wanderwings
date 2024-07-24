1f454674a789c2bdf135668962ec3bc5c6717293
Alphaeu <dealphaeus@gmail.com>
2024-07-24 13:39:47 +0300

    Update config/allowedOrigins and server file

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+// config/allowedOrigins.js
+
+const allowedOrigins = [
+    'https://easy-travel-frontend.netlify.app',
+  ];
+  
+  module.exports = allowedOrigins;
+  
--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/errorHandler');
 const authRoutes = require('./routes/authRoutes');
 const flightRoutes = require('./routes/flightRoutes');
 const userRoutes = require('./routes/userRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
+const allowedOrigins = require('./config/allowedOrigins');
 
 dotenv.config();
 
 const app = express();
+
+// CORS options
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
 
 // Connect Database
 connectDB();
@@ -19,7 +37,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/flights', flightRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/payments', paymentRoutes); // Corrected line
+app.use('/api/payments', paymentRoutes);
 
 // Error Handling Middleware
 app.use(errorHandler);
@@ -29,4 +47,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
 
