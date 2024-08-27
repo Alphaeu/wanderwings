8ad32c3cae6913c5b119d3d796dd7599106c644f
Alphaeu <dealphaeus@gmail.com>
2024-08-27 20:00:19 +0300

    Update allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,27 @@
 // config/allowedOrigins.js
 
-const allowedOrigins = [
-    'https://easy-travel-frontend.netlify.app',
-  ];
-  
-  module.exports = allowedOrigins;
-  
+// To load our environment variables
+require('dotenv').config();
+
+const devOrigins = [
+  'http://localhost:3000', // Localhost for frontend development
+  'http://localhost:8080', // Localhost for API development/testing
+];
+
+const prodOrigins = [
+  'https://wanderwings.netlify.app', // Production frontend
+  'https://api.wanderwings.com', // Production API
+];
+
+// Determine the environment
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Set allowed origins based on the environment
+const allowedOrigins = isProduction ? prodOrigins : devOrigins;
+
+// Additional dynamic origins go here
+if (process.env.ALLOWED_ORIGINS) {
+  allowedOrigins.push(...process.env.ALLOWED_ORIGINS.split(','));
+}
+
+module.exports = allowedOrigins;
